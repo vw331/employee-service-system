47db45ff5839e35a09c3d8b859de3dcc75eb8a1e
vw331 <1296502722@qq.com>
2020-08-01 12:49:20 +0800

    to mac

--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ const routes = [
     children: [
       {
         path: '/main',
-        name: 'MainIndex',
+        //name: 'MainIndex',
         component: () => import('@/views/main'),
         children: [
           {
